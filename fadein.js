cb772d57b499a2f493426519f98a7ca3cf5efda9
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2022-06-30 20:05:59 +0900

    Change fadein animation into css elements.

--- fadein.js
+++ fadein.js
@@ -49,21 +49,6 @@ function init() {
             for(var i = 0;i < 10;i++){
                 fadein2d.fillRect(canvasWidth*(i/20+1-Math.max(0,1-(t-0.8))),0,canvasWidth,canvasHeight);
             }
-/*
-            if(drawTxtTemp=="ABOUT"){
-                fadein2d.fillStyle="rgba(0,0,0,"+ Math.min(1,Math.max(0,1-(t-0.8)))/3+")";
-                for(var i = 0;i < 10;i++){
-                    fadein2d.fillRect(0,canvasHeight*(i/20+1-Math.max(0,1-(t-0.8))),canvasWidth,canvasHeight);
-                }
-            } else if(drawTxtTemp=="WORKS"){ 
-                fadein2d.fillStyle="rgba(0,0,0,"+ Math.min(1,Math.max(0,1-(t-0.8)))/3+")";
-                for(var i = 0;i < 10;i++){
-                    fadein2d.fillRect(canvasWidth*(i/20+1-Math.max(0,1-(t-0.8))),0,canvasWidth,canvasHeight);
-                }
-            } else {
-                fadein2d.fillRect(0,0,canvasWidth,canvasHeight);
-                fadein2d.fillRect(0,0,canvasWidth*(t-0.8),canvasHeight);
-            }   */ 
         } else{
             fadein2d.fillRect(0,0,canvasWidth,canvasHeight);
         }
@@ -81,7 +66,7 @@ function init() {
 
         let fadein_fontsize=18;
         fadein2d.font=fadein_fontsize+"mm "+"Poiret One, cursive";
-        fadein2d.fillStyle="rgba(255,255,255,"+ Math.min(1,Math.max(0,1-(t-0.8)/1))+")";
+        fadein2d.fillStyle="rgba(255,255,255,"+ Math.min(1,Math.max(0,1-(t-0.8)/3))+")";
         drawTxtTemp="";
         if(window.location.href.substr(-10) == "about.html"){ 
             drawTxtTemp="ABOUT";
@@ -92,12 +77,13 @@ function init() {
         }  else if(window.location.href.substr(-11) == "typing.html"){ 
             drawTxtTemp="TYPING";
         } 
-        var drawTxt=drawTxtTemp.substr(0,Math.floor(t*10));
-        inputChar=String.fromCharCode(65+Math.floor(Math.random()*25));
-        if(0<Math.max(0,Math.floor(t*10)) && Math.max(0,Math.floor(t*10))<drawTxtTemp.length) drawTxt+=inputChar;
-        fadein2d.fillText(drawTxt,(canvasWidth-fadein2d.measureText(drawTxtTemp).width)/2,canvasHeight/2);
-
-        if(t>2.8 && nextPage=="") document.getElementById("fadeinCanvas").style.zIndex=12;
+        var drawTxt=drawTxtTemp.substr(0,Math.floor(t*12));
+        document.getElementById("fadeinText").textContent=drawTxt;
+        document.getElementById("fadeinText").style.color="rgba(255,255,255," + Math.max(Math.min(1,1-(t-0.4)/0.8)) +")";
+        if(t>2.8 && nextPage=="") {
+            document.getElementById("fadeinCanvas").style.zIndex=12;
+            document.getElementById("fadeinText").style.zIndex=-1;
+        }
         if(nextPage!=""){ //遷移時
             let t2=performance.now()-nextPageLP;
             if(t2 > 1000) location.href=nextPage;
